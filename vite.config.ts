0e65d8d45110ba6690972e77672215166d275cc4
9huit <nantenkf@gmail.com>
2025-03-19 11:06:23 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['path', 'fs', 'url', 'stream', 'http2', 'zlib', 'querystring'], // Mark server-side modules as external
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
