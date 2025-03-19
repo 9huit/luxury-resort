d88ad5bc78909684821bdb74e8be6d99cd9af469
9huit <nantenkf@gmail.com>
2025-03-19 11:59:37 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import polyfillNode from 'rollup-plugin-polyfill-node'; // Ajout du polyfill
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    polyfillNode() // Ajouter le plugin de polyfill
-  ],
-  build: {
-    sourcemap: false, // Désactiver les sourcemaps
-    rollupOptions: {
-      external: [
-        'path', 'fs', 'url', 'stream', 'http2', 'zlib', 'querystring',
-        'util', 'os', 'child_process', 'net', 'tls', 'assert', 'crypto', 
-        'buffer', 'http', 'https', 'node:events', 'node:process', 'node:util'
-      ], // Mark server-side modules as external
-    },
-  },
+  plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
